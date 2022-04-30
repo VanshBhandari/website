af13aa5dd18fbb22afe8ac673b01d12556bb8b32
vansh <71205949+VanshBhandari@users.noreply.github.com>
2022-04-30 00:25:25 -0700

    smol changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
+import { FaChevronDown } from 'react-icons/fa'
+import { FaChevronUp } from 'react-icons/fa'
 import ParticleBackground from './ParticleBackground'
 
 function App() {
   return (
     <div>
       <ParticleBackground/> 
-      <div class = "container">
+      <div class = "container-1" id = "section-1">
         <span class="wave"> 👋</span>Nice to meet you, I'm<span class ="name">Vansh</span>
       </div>
       <div class = "subheader">
@@ -15,6 +17,9 @@ function App() {
       </div>
       <div class = "links">
       <p><a href="https://www.linkedin.com/in/vanshbhandari/" target="_blank"><FaLinkedin size={35} /></a>   <a href = "mailto: vbhandari2005@gmail.com"><MdEmail size={35}/></a>   <a href = "https://github.com/VanshBhandari"target="_blank"><FaGithub size={35}/></a></p>  
+      </div>
+      <div class = "scroll">
+        <a href='#section-2'></a>
       </div>
     </div>
   );
